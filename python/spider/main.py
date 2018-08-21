24626afa25772b554042f1fba7188150b8969917
hzh <hao9456@qq.com>
2018-08-21 22:22:30 +0800

    20180821提交

--- python/spider/main.py
+++ python/spider/main.py
@@ -8,28 +8,27 @@ from urllib import request
 from bs4 import BeautifulSoup
 
 
-def main():
-    download_url = 'http://www.biqukan.com/1_1452/7600592.html'
+def get_article():
+    download_url = 'http://www.biqukan.com/1_1094/5403177.html'
     # head = {}
-    # head['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.5702.400 QQBrowser/10.2.1893.400'
-    # head['Host'] = 'www.biqukan.com'
-    download_req = request.Request(url=download_url, headers=head)
+    # head['User-Agent'] = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19'
+    download_req = request.Request(url=download_url)
     download_response = request.urlopen(download_req)
-    download_html = download_response.read().decode('gbk', 'ignore')
-    print(1,download_html)
-    # url = 'https://api.github.com/some/endpoint'
-    # payload = {'some': 'data'}
-    # headers = {'content-type': 'application/json'}
-    # r = requests.post(url, data=json.dumps(payload), headers=headers)
-    # print
-    # r.text
+    dd = download_response.read()
+    # download_html = dd.replace(b'\r\n',b'\r').replace(b'\r',b'\r\n').decode('gbk', 'ignore')
+    download_html = dd.replace(b'\n',b'').replace(b'\r',b'\r\n').decode('gbk', 'ignore')
+    soup_texts = BeautifulSoup(download_html, 'lxml')
+    texts = soup_texts.find_all(id='content', class_='showtxt')
+    soup_text = BeautifulSoup(str(texts), 'lxml')
+    # 将\xa0无法解码的字符删除(这个其实就是网页中的&nbsp;)
+    # print(dd.replace(b'\r',b'').decode('gbk', 'ignore'))
+    # print(texts)
+    print(soup_text.div.text.replace('\xa0', ''))
 
 
-    # soup_texts = BeautifulSoup(download_html, 'lxml')
-    # texts = soup_texts.find_all(id='content', class_='showtxt')
-    # soup_text = BeautifulSoup(str(texts), 'lxml')
-    # # 将\xa0无法解码的字符删除
-    # print(soup_text.div.text.replace('\xa0', ''))
+def main():
+    get_article()
+
 
 
 if __name__ == '__main__':
--- python/spider/try_get.py
+++ python/spider/try_get.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author: HZH create at 2018/8/20 21:04 
-# Description: 
+# Description:
 
-import requests
+
+from urllib import request
+from bs4 import BeautifulSoup
+
+
+def get_comics_list():
+    download_url = 'http://m.bzku520.com/shaonvmanhua/'
+    # head = {}
+    # head['User-Agent'] = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19'
+    download_req = request.Request(url=download_url)
+    download_response = request.urlopen(download_req)
+    download_response_data = download_response.read()
+    # download_html = download_response_data.replace(b'\r\n',b'\r').replace(b'\r',b'\r\n').decode('gbk', 'ignore')
+    download_html = download_response_data.decode('gbk', 'ignore')
+    # print(download_html)
+    soup_texts = BeautifulSoup(download_html, 'lxml')
+    texts = soup_texts.find_all('img')
+    soup_text = BeautifulSoup(str(texts), 'lxml')
+    print(texts)
+    # 将\xa0无法解码的字符删除(这个其实就是网页中的&nbsp;)
+    # print(download_response_data.replace(b'\r',b'').decode('gbk', 'ignore'))
+    # print(texts)
+    # print(soup_text.div.text.replace('\xa0', ''))
 
 
 def main():
-    # download_url = 'http://www.biqukan.com/1_1452/7600592.html'
-    # url2 = "https://github.com/timeline.json"
-    # headers = {'content-type': 'application/json'}
-    # res = requests.get(download_url,headers = headers)
-    # print(res.text)
-    url = "http://www.biqukan.com/1_1094/5403177.html"
-    headers = {
-        'cache-control': "no-cache",
-        'postman-token': "f0c3b6cf-912d-03df-4c38-6c6788690ac7",
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.5702.400 QQBrowser/10.2.1893.400',
-    }
-    response = requests.request("GET", url, headers=headers)
-    print(response.text)
+    get_comics_list()
 
 
 if __name__ == '__main__':
