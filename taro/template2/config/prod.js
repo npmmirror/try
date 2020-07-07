8bb260e58b947e154a22710b40b381b185007254
Woz Huang <909695699@qq.com>
2020-07-07 14:32:26 +0800

    feat: taro V3 模板添加 webpack-bundle-analyzer

--- taro/template2/config/prod.js
+++ taro/template2/config/prod.js
@@ -4,7 +4,13 @@ module.exports = {
   },
   defineConstants: {
   },
-  mini: {},
+  mini: {
+    // // 可以用来分析打包后的大小
+    // webpackChain(chain, webpack) {
+    //   chain.plugin('analyzer')
+    //     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin, [])
+    // }
+  },
   h5: {
     /**
      * 如果h5端编译后体积过大，可以使用webpack-bundle-analyzer插件对打包体积进行分析。
