a7bb5e757eb5c098348f55e06340345f3bc57490
Woz Huang <909695699@qq.com>
2021-06-07 17:33:53 +0800

    瓦片地图url测试

---
+++ map/data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    applyMapUrl: 'https://wprd01.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=2&style=8&ltype=11',
+    mapName: '高德路网',
+  },
+  {
+    applyMapUrl: 'https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
+    mapName: '高德影像',
+  },
+  {
+    applyMapUrl: 'https://wprd04.is.autonavi.com/appmaptile?appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}',
+    mapName: '高德矢量',
+  },
+];
