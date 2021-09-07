12ae0454b0f25e75755a38c0fe8317854bebd02c
Woz Huang <909695699@qq.com>
2021-09-07 16:40:31 +0800

    feat: 获取截图后自动打开

--- nodejs/pkg/index.js
+++ nodejs/pkg/index.js
@@ -1,5 +1,7 @@
 const download = require('download');
+const path = require('path');
 const qs = require('qs');
+const open = require('open');
 
 // 使用 wordpress 对网页生成预览图
 
@@ -16,9 +18,13 @@ if (process.argv[2]) {
   try {
     // const res = await axios.get();
     const u = wpUrl + encodeURIComponent(url) + qs.stringify({ w, h }, { addQueryPrefix: true });
-    const res = await download(u, 'temp', {
-      filename: Date.now() + '-' + url.replace('https://', '') + '.png',
+    const destination = 'temp';
+    const filename = Date.now() + '-' + url.replace('https://', '') + '.png';
+    const res = await download(u, destination, {
+      filename,
     });
+    await open(path.resolve(destination, filename));
+    await open(url);
     // console.log(res);
   } catch (err) {
     console.error(err);
